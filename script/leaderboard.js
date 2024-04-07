dd1fdfde596d33694e08392058b3bc59e4d40110
Manpreet Singh <63737630+ManpreetXSingh@users.noreply.github.com>
2024-04-07 14:18:01 +0530

    FIx bugs, format code

--- script/leaderboard.js
+++ script/leaderboard.js
@@ -1,26 +1,24 @@
-fetch("./data.json")
+const leaderboardTable = document.getElementById("leaderboard");
+const leaderboardBody = leaderboardTable.getElementsByTagName("tbody")[0];
+var leaderboard = null;
+
+fetch("data/leaderboard.json")
   .then((response) => response.json())
   .then((data) => {
-    const studentData = data;
-    console.log(studentData);
+    leaderboard = Object.values(data).sort((a, b) => b.score - a.score);
 
-    const studentArray = Object.values(studentData).sort(
-      (a, b) => b.points - a.points
-    );
-
-    studentArray.forEach((student, index) => {
+    leaderboard.forEach((student, index) => {
       student.rank = index + 1;
-    
-      let tableRow  = document.createElement("tr");
+
+      let tableRow = document.createElement("tr");
       tableRow.innerHTML = `
         <td>${student.rank}</td>
         <td>${student.name}</td>
         <td>${student.sem}</td>
         <td>${student.language}</td>
-        <td>${student.points}</td>`;
-      
-      let parent = document.getElementsByClassName("table")[0];
-      parent.append(tableRow);
+        <td>${student.score}</td>`;
+
+      leaderboardBody.append(tableRow);
     });
   })
   .catch((error) => console.error("Error fetching data:", error));
